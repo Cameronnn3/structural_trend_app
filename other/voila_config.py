58bcbfcd62bead67b3eaeaa04011896047dba42c
Cameronnn3 <155463393+Cameronnn3@users.noreply.github.com>
2025-06-09 16:33:03 -0700

    Delete other directory

--- other/voila_config.py
+++
@@ -1,6 +0,0 @@
-from traitlets.config import Config
-c = Config()
-# show Python tracebacks in the browser
-c.VoilaConfiguration.show_tracebacks = True
-# also turn on debug logging
-c.VoilaConfiguration.debug = True
